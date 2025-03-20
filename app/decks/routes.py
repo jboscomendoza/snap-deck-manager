0cabcc865bfc7f145e10bf5b82103c7f638dc6b0
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-03-19 18:55:07 -0600

    Card view, 404 error

--- app/decks/routes.py
+++ app/decks/routes.py
@@ -1,10 +1,24 @@
 from config import basedir
-from flask import render_template, request, redirect, flash, url_for
+from flask import (
+    render_template, request, redirect, flash, url_for, abort
+    )
 from app.extensions import db
 from app.decks import bp
 from app.forms.decks import FormDecks
 from app.models.decks import Deck, Tag
 import re
+import json
+from os import path
+
+
+PATH_CARDS_JSON = path.join(
+    "app",
+    "marvel-snap-cards",
+    "marvel-snap-cards.json"
+    )
+
+with open(PATH_CARDS_JSON, mode="r") as f:
+    cards = json.load(f)
 
 @bp.route("/", methods=["GET", "POST"])
 def index():
@@ -62,21 +76,49 @@ def get_decks():
         decks=decks,
     )
 
-@bp.route("/view-decks/<int:id>", methods=["GET", "POST"])
-def view_decks(id):
+@bp.route("/deck/<int:id>/", methods=["GET", "POST"])
+def view_deck(id):
     deck = db.first_or_404(
         db.select(Deck).filter_by(id=id)
     )
     raw_decklist = deck.decklist
     raw_decklist = raw_decklist.split("#")
     # 1:12 is the position of the card names in a standard decklist
-    decklist = [re.sub("\(.\) ", "", i) for i in raw_decklist[1:12]]
+    decklist = [re.sub(r"\(.\) ", "", i) for i in raw_decklist[1:12]]
+    decklist = [i.strip() for i in decklist]
     # 23 is the position of the deck code in a standard decklist
     deck_code = raw_decklist[13]
     return render_template(
-        "view-decks.html",
+        "deck.html",
         title="Deck: " + deck.name,
         deck=deck,
         decklist=decklist,
         deck_code=deck_code,
     )
+
+@bp.route("/card/<string:cardname>")
+def view_card(cardname):
+    title = cardname
+    for i in cards:
+        if i["name"] == cardname:
+            card_data = i
+            break
+        else:
+            card_data = None
+    keys = ("energy", "power", "ability", "series")
+    if not card_data:
+        return abort(404)
+    return render_template(
+        "card.html",
+        title=title,
+        card_data=card_data,
+        keys=keys,
+    )
+
+@bp.errorhandler(404)
+def page_not_found(e):
+    title="Page not found"
+    return render_template(
+        "404.html",
+        title=title,
+        )
