4d7a72bae0cb5360068a785b97b2ee054ce4a0e8
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-04-02 18:20:14 -0600

    Deck and tag counts

--- app/decks/routes.py
+++ app/decks/routes.py
@@ -1,33 +1,28 @@
-from config import basedir
-from flask import (
-    render_template, request, redirect, flash, url_for, abort
-    )
+from flask import render_template, request, redirect, flash, url_for, abort
 from app.extensions import db
 from app.decks import bp
 from app.forms.decks import FormDecks, DeleteDeck
-from app.models.decks import Deck, Tag, deck_tag
+from app.models.decks import Deck, Tag
 import re
 import json
 from os import path
 
 
-PATH_CARDS_JSON = path.join(
-    "app",
-    "marvel-snap-cards",
-    "marvel-snap-cards.json"
-    )
+PATH_CARDS_JSON = path.join("app", "marvel-snap-cards", "marvel-snap-cards.json")
 UNIQUE_ENERGY = [str(i) for i in range(9)]
 
+
 def parse_cards(raw_decklist: str) -> list:
-    '''Returns a CARD list from  raw DECKLIST.'''
+    """Returns a CARD list from  raw DECKLIST."""
     decklist = raw_decklist.split("#")
     # 1:12 is the position of the card names in a standard decklist
     decklist = [re.sub(r"\(.\) ", "", i) for i in decklist[1:12]]
     decklist = [i.strip() for i in decklist]
     return decklist
 
+
 def parse_energy(raw_decklist: str, unique_energy: list) -> dict:
-    '''Returns an ENERGY dict from  raw DECKLIST.'''
+    """Returns an ENERGY dict from  raw DECKLIST."""
     split_decklist = raw_decklist.split("#")
     # Finds numeric values betwee parentheses, denoting energy values
     raw_energy = [re.findall(r"\((\d)\)", i)[0] for i in split_decklist[1:12]]
@@ -35,33 +30,44 @@ def parse_energy(raw_decklist: str, unique_energy: list) -> dict:
     energy = dict(zip(unique_energy, count_energy))
     return energy
 
+
 with open(PATH_CARDS_JSON, mode="r") as f:
     cards = json.load(f)
 
+
 @bp.route("/", methods=["GET", "POST"])
 def index():
-    title="Home"
+    title = "Summary"
+    deck_count = db.session.execute(
+        db.func.count(Deck.id)
+    ).scalar()
+    tag_count = db.session.execute(
+        db.func.count(Tag.id)
+    ).scalar()
     urls = {
-        "Add decks":"add_decks",
-        "Get decks":"get_decks",
-        "Get tags" :"get_tags",
-        }
+        "Add decks": "add_decks",
+        "Get decks": "get_decks",
+        "Get tags": "get_tags",
+    }
     return render_template(
         "index.html",
         title=title,
         urls=urls,
-        )
+        deck_count=deck_count,
+        tag_count=tag_count,
+    )
+
 
 @bp.route("/add-decks/", methods=["GET", "POST"])
 def add_decks():
     title = "Add new deck"
-    form  = FormDecks()
+    form = FormDecks()
     if form.validate_on_submit() and request.method == "POST":
-        deck_name     = request.form["name"]
+        deck_name = request.form["name"]
         deck_decklist = request.form["decklist"]
         deck = Deck(
-            name     = deck_name,
-            decklist = deck_decklist,
+            name=deck_name,
+            decklist=deck_decklist,
         )
         tag_names = request.form["tag"]
         tag_names = [i.strip() for i in tag_names.split(",")]
@@ -78,29 +84,27 @@ def add_decks():
         db.session.add(deck)
         db.session.commit()
         flash(f"Deck {deck_name} added.")
-        return redirect(url_for(
-            "decks.get_decks"
-        ))
-    edit=False
+        return redirect(url_for("decks.get_decks"))
     return render_template(
         "add-decks.html",
         title=title,
         form=form,
     )
 
+
 @bp.route("/get-decks/", methods=["GET", "POST"])
 def get_decks():
-    title="Decks"
+    title = "Decks"
     decks = db.session.query(Deck)
     deck_items = []
     for i in decks:
         decklist = i.decklist.split("#")
         deck_cards = decklist[1:12]
         deck_dict = {
-            "name"  : i.name,
-            "cards" : "".join(deck_cards),
-            "code"  : decklist[13],
-            "tag"   : i.tags,
+            "name": i.name,
+            "cards": "".join(deck_cards),
+            "code": decklist[13],
+            "tag": i.tags,
         }
         deck_items.append(deck_dict)
     return render_template(
@@ -110,34 +114,32 @@ def get_decks():
         decks=decks,
     )
 
+
 @bp.route("/delete-deck/<string:id>", methods=["GET", "POST"])
 def delete_deck(id: int):
     form = DeleteDeck()
     request.args.get("delete")
-    deleted_deck = db.first_or_404(
-        db.select(Deck).filter_by(id=id)
-    )
+    deleted_deck = db.first_or_404(db.select(Deck).filter_by(id=id))
     if request.method == "POST" and request.form["confirm"] == "Yes":
-        db.session.execute(
-            db.delete(Deck).filter_by(id=deleted_deck.id)
-            )
+        db.session.execute(db.delete(Deck).filter_by(id=deleted_deck.id))
         db.session.commit()
         flash(f"Deck {deleted_deck.name} deleted!")
-        return redirect(url_for(
-            "decks.get_decks"
-            ))
+        return redirect(url_for("decks.get_decks"))
     elif request.method == "POST" and request.form["confirm"] == "No":
-        return redirect(url_for(
-            "decks.view_deck",
-            id=deleted_deck.id,
-            ))
+        return redirect(
+            url_for(
+                "decks.view_deck",
+                id=deleted_deck.id,
+            )
+        )
     title = f"Delete deck: {deleted_deck.name}?"
     return render_template(
         "delete-deck.html",
         title=title,
         deleted_deck=deleted_deck,
         form=form,
-        )
+    )
+
 
 @bp.route("/tag/", methods=["GET"])
 @bp.route("/tag/<string:tag>/", methods=["GET"])
@@ -145,8 +147,11 @@ def get_tags(tag=None):
     if not tag:
         title = "Tags"
         tags = db.session.execute(
-            db.select(Tag)
-        ).scalars()
+            db.select(Tag.name, db.func.count(Deck.id))
+            .join(Deck.tags)
+            .distinct()
+            .group_by(Tag.id)
+        )
         return render_template(
             "get-tags.html",
             tags=tags,
@@ -154,29 +159,28 @@ def get_tags(tag=None):
         )
     else:
         title = f"Decks with tag: {tag}"
-        decks = db.session.execute(
-            db.select(Deck)
-            .join(Deck.tags)
-            .filter(Tag.name == tag)
-        ).scalars().unique()
+        decks = (
+            db.session.execute(db.select(Deck).join(Deck.tags).filter(Tag.name == tag))
+            .scalars()
+            .unique()
+        )
         return render_template(
             "get-decks.html",
             title=title,
             decks=decks,
-            )
+        )
+
 
 @bp.route("/deck/<int:id>", methods=["GET", "POST"])
 def view_deck(id):
-    deck = db.first_or_404(
-        db.select(Deck).filter_by(id=id)
-    )
+    deck = db.first_or_404(db.select(Deck).filter_by(id=id))
     # Tags as a single string
     current_tags = ", ".join([i.name for i in deck.tags])
     edit = request.args.get("edit")
     if edit and request.method == "POST":
-        deck.name     = request.form["name"]
+        deck.name = request.form["name"]
         deck.decklist = request.form["decklist"]
-        tag_names     = request.form["tag"]
+        tag_names = request.form["tag"]
         tag_names = [i.strip() for i in tag_names.split(",")]
         # Remove tags no longer listed
         for tag in deck.tags:
@@ -190,16 +194,18 @@ def view_deck(id):
             if not tag_in_db:
                 new_tag = Tag(name=tag_name)
                 deck.tags.append(new_tag)
-            elif tag_in_db and not(tag_in_db in deck.tags):
+            elif tag_in_db and (tag_in_db not in deck.tags):
                 deck.tags.append(tag_in_db)
             elif tag_in_db and (tag_in_db in deck.tags):
                 pass
         db.session.commit()
         flash("Deck updated!")
-        return redirect(url_for(
-            "decks.view_deck",
-            id=deck.id,
-            ))
+        return redirect(
+            url_for(
+                "decks.view_deck",
+                id=deck.id,
+            )
+        )
     if edit:
         title = f"Editing deck: {deck.name}"
         form = FormDecks(
@@ -213,19 +219,20 @@ def view_deck(id):
             deck=deck,
             form=form,
         )
-    raw_decklist = deck.decklist    
+    raw_decklist = deck.decklist
     decklist = parse_cards(raw_decklist)
     energy = parse_energy(raw_decklist, UNIQUE_ENERGY)
     # 13 is the position of the deck code in a standard decklist
     deck_code = raw_decklist.split("#")[13]
     return render_template(
         "deck.html",
-        title="Deck: " + deck.name,
+        title=f"Deck: {deck.name}",
         deck=deck,
         decklist=decklist,
         deck_code=deck_code,
         energy=energy,
     )
+
 
 @bp.route("/card/<string:cardname>")
 def view_card(cardname):
@@ -246,10 +253,11 @@ def view_card(cardname):
         keys=keys,
     )
 
+
 @bp.errorhandler(404)
 def page_not_found(e):
-    title="Page not found"
+    title = "Page not found"
     return render_template(
         "404.html",
         title=title,
-        )
+    )
